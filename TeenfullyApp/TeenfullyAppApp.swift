bf763af610eb895100eaa7f79db33780b95964b4
galois777 <galois77777@gmail.com>
2023-07-19 20:08:14 -0500

    Initial Commit

---
+++ TeenfullyApp/TeenfullyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeenfullyAppApp.swift
+//  TeenfullyApp
+//
+//  Created by 张博诚 on 7/19/23.
+//
+
+import SwiftUI
+
+@main
+struct TeenfullyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
