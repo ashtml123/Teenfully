dd269d37b35aba3c29e247e849606fb11bc8d604
galois777 <galois77777@gmail.com>
2023-07-24 20:20:36 -0500

    Added TabBar

--- TeenfullyApp/View/BottomNavigation/BottomBar.swift
+++ TeenfullyApp/View/BottomNavigation/BottomBar.swift
@@ -4,21 +4,13 @@ import SwiftUI
 
 struct BottomBar: View {
     var body: some View {
-        ZStack {
-            Rectangle()
-                .foregroundColor(.clear)
-                .frame(width: 375, height: 85)
-                .background(.white)
-                .shadow(
-                    color: Color(red: 0, green: 0, blue: 0, opacity: 0.11), radius: 12, y: 4
-                )
-            ZStack {
-                Image("HomeIcon")
-                    .offset(x:-90)
-                Image("GroupIcon")
-                    .offset(x:0)
-            }
+            TabView {
+                HomeScreenView()
+                    .tabItem(){
+                        Image("HomeIcon")
+                    }
         }
+            .frame(width: 375,height:20)
     }
 }
 
