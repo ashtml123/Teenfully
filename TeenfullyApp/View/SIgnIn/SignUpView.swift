cbc49270d8af78dd4cce61276f0efccdf7ba927c
Rohan Tamhankar <curiousrohan1@gmail.com>
2023-08-03 12:32:55 -0700

    * Added sign up part I

--- TeenfullyApp/View/SIgnIn/SignUpView.swift
+++ TeenfullyApp/View/SIgnIn/SignUpView.swift
@@ -8,32 +8,45 @@ import FirebaseAuth
 import GoogleSignIn
 import GoogleSignInSwift
 
-
-struct SignInView: View {
+enum AuthenticationState {
+    case unauthenticated
+    case authenticating
+    case authenticated
+}
+enum AuthenticationError: Error {
+    case tokenError(message: String)
+}
+struct SignUpView: View {
+    @State var firstname: String = ""
+    @State var lastname: String = ""
+    @State var age: String = ""
     @State var email: String = ""
     @State var password: String = ""
     @State var authenticated = false
+    @State var authenticationState: AuthenticationState = .unauthenticated
     @State var failed = false
     @State var errorMessage: String = ""
     @Environment(\.dismiss) var dismiss
-    func getHere(){
-        print("Got here")
+    func signUpWithEmailPassword() async -> Bool {
+        authenticationState = .authenticating
+        do  {
+            try await Auth.auth().createUser(withEmail: email, password: password)
+            return true
+        }
+        catch {
+            print(error)
+            errorMessage = error.localizedDescription
+            authenticationState = .unauthenticated
+            return false
+        }
     }
-    func login() {//TODO: Fix the failure to login; right now, when we log in with invalid credentials, it does nothing, but it should give an error.
-        Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
-            if error != nil {
-                failed = true
-                print(error?.localizedDescription ?? "")
-            } else {
-                failed = false
-                print("success")
-                authenticated=true
-                print("User \( result?.user.uid) signed in")
-                
+    private func sWithEmailPassword() -> Void{
+        Task {
+            if await signUpWithEmailPassword() == true {
+                dismiss()
             }
         }
     }
-    
     private func sWithGoogle() ->Void{
         Task {
             if await signInWithGoogle() == true {
@@ -96,7 +109,7 @@ struct SignInView: View {
                         .background(.white)
                         .cornerRadius(23)
                         .offset(x: 0, y: 25.50)
-                    Text("Login to your Account")
+                    Text("Make an Account")
                         .font(Font.custom("Inter", size:21).weight(.bold))
                         .foregroundColor(Color(red: 0.10, green: 0.10, blue: 0.10))
                         .offset(x: -41, y: -235.50)
@@ -110,44 +123,33 @@ struct SignInView: View {
                         .overlay(
                             RoundedRectangle(cornerRadius: 5)
                                 .inset(by: 0.50)
-                                .stroke(Color(red: 0.61, green: 0.61, blue: 0.61), lineWidth: 0.50)
-                        )
-                        .offset(x: -0.50, y: -5)
-                    Rectangle()
-                        .foregroundColor(.clear)
-                        .frame(width: 271, height: 42)
-                        .cornerRadius(5)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 5)
-                                .inset(by: 0.50)
                                 .stroke(Color(red: 0.85, green: 0.85, blue: 0.85), lineWidth: 0.50)
                         )
                         .offset(x: -0.50, y: -145)
-                    Rectangle()
-                        .foregroundColor(.clear)
-                        .frame(width: 271, height: 42)
-                        .cornerRadius(5)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 5)
-                                .inset(by: 0.50)
-                                .stroke(Color(red: 0.85, green: 0.85, blue: 0.85), lineWidth: 0.50)
-                        )
-                        .offset(x: -0.50, y: 48)
-                    TextField("Email", text: $email)
-                        .offset(x:100,y:2)
-                    
-                    SecureField("Password", text: $password)
-                        .offset(x:100,y:50)
-                };Group {
+                    VStack{
+                        TextField("First Name", text: $firstname)
+                            .textFieldStyle(.roundedBorder)
+                        TextField("Last Name", text: $lastname)
+                            .textFieldStyle(.roundedBorder)
+                        TextField("Email", text: $email)
+                            .textFieldStyle(.roundedBorder)
+                        TextField("Age", text: $age)
+                            .textFieldStyle(.roundedBorder)
+                        SecureField("Password", text: $password)
+                            .textFieldStyle(.roundedBorder)
+                    }
+                    .offset(x:0,y:25)
+                    .frame(width: 368, height: 200)
+                }
+                Group {
                     NavigationLink {
-                        SignUpView()
+                        SignInView()
                     } label: {
-                        Text("Don’t have an account? Sign Up")
+                        Text("Already have an account? Sign In")
                             .font(Font.custom("Inter", size: 12.60))
                             .foregroundColor(Color(red: 0.04, green: 0.04, blue: 0.04))
+                            .offset(x: -8, y: 200)
                     }
-                    .offset(x: -8, y: 163.50)
-
                     Button(action: {sWithGoogle()}) {
                         Image("GoogleLogo")
                             .resizable()
@@ -161,20 +163,18 @@ struct SignInView: View {
                         .scaledToFit()
                         .frame(height: 25,alignment: .center)
                         .offset(x: 30,y: -145)
-                    VStack() {
+                    ZStack() {
                         Rectangle()
                             .foregroundColor(.clear)
                             .frame(width: 271, height: 42)
                             .background(Color(red: 1, green: 0.51, blue: 0.21))
                             .cornerRadius(5)
-                            .offset(x: 0, y: 0)
-                        Button(action: {login()}) {
-                            Text("Log In")
+                        Button(action: {sWithEmailPassword()}) {
+                            Text("Sign Up")
                         }
-                        
                     }
                     .frame(width: 271, height: 42)
-                    .offset(x: -0.50, y: 122)
+                    .offset(x: -0.50, y: 160)
                     Rectangle()
                         .foregroundColor(.clear)
                         .frame(width: 271, height: 0)
@@ -201,11 +201,11 @@ struct SignInView: View {
                                 .inset(by: 0.92)
                                 .stroke(Color(red: 0.85, green: 0.85, blue: 0.85), lineWidth: 0.92)
                         )
-                        .offset(x: 0, y: -75)
+                        .offset(x: 0, y: -100)
                     Text("Or")
                         .font(Font.custom("Inter", size: 13.60).weight(.medium))
                         .foregroundColor(Color(red: 0.84, green: 0.83, blue: 0.85))
-                        .offset(x: 0, y: -75)
+                        .offset(x: 0, y: -100)
                 };Group {
                     ZStack() { }
                         .frame(width: 18, height: 18)
@@ -219,9 +219,9 @@ struct SignInView: View {
     }
 }
 
-struct SignInView_Previews: PreviewProvider {
+struct SignUpView_Previews: PreviewProvider {
     static var previews: some View {
-        SignInView()
+        SignUpView()
     }
 }
 
