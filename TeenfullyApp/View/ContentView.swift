059936e64197a2dc752b774f21370c04488aa4e7
Rohan Tamhankar <curiousrohan1@gmail.com>
2023-08-15 14:39:18 -0700

    * Added the disclaimer.

--- TeenfullyApp/View/ContentView.swift
+++ TeenfullyApp/View/ContentView.swift
@@ -1,32 +1,52 @@
-//
-
 import SwiftUI
+
+struct DisclaimerView: View {
+    @Binding var isPresented: Bool
+    
+    var body: some View {
+        VStack {
+            Text("Teenfully is run by a team of teenagers. Teenfully content is not a substitute for professional medical or psychological advice.")
+                .multilineTextAlignment(.center)
+                .padding()
+            
+            Text("Always seek the guidance of a qualified adult professional for any concerns you have.")
+                .multilineTextAlignment(.center)
+                .padding()
+            
+            Button("OK") {
+                isPresented = false
+            }
+            .padding()
+        }
+    }
+}
 
 struct ContentView: View {
     @State private var name = ""
     @State private var age = 0
-    @State private var articleHolder: [Article]=[]
+    @State private var articleHolder: [Article] = []
     @State private var profileImageURL: String = ""
+    @State private var isDisclaimerPresented = true // To control the presentation of the disclaimer
+    
     var body: some View {
         TabView {
             HomeScreenView(name: self.name, age: self.age, articleHolder: self.articleHolder)
-                .tabItem(){
+                .tabItem() {
                     Label("Home", image: "HomeIcon")
                 }
-
-            MeditateView()
-                .tabItem(){
-                    Label("Meditate",image:"MeditationIcon")
-                }
-            ProfileInterface(profileImageURL:self.profileImageURL, name:self.name,age:self.age)
-                .tabItem(){
-                    Label("Profile",systemImage: "person.crop.circle")
-                }
-
-           
             
+            MeditateView()
+                .tabItem() {
+                    Label("Meditate", image: "MeditationIcon")
+                }
+            
+            ProfileInterface(profileImageURL: self.profileImageURL, name: self.name, age: self.age)
+                .tabItem() {
+                    Label("Profile", systemImage: "person.crop.circle")
+                }
         }
         .onAppear {
+            // Fetch user profile and articles
             FirebaseManager.shared.fetchUserProfile { userProfile in
                 if let userProfile = userProfile {
                     DispatchQueue.main.async {
@@ -48,6 +68,13 @@ struct ContentView: View {
                     articleHolder=fetchedArticles!
                 }
             }
+            // Present the disclaimer modal
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                isDisclaimerPresented = true
+            }
+        }
+        .sheet(isPresented: $isDisclaimerPresented) {
+            DisclaimerView(isPresented: $isDisclaimerPresented)
         }
     }
 }
